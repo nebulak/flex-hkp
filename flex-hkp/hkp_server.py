e5ff99a64321bd66a6d50d3c3540826c8bb96646
nebulak <nebulak@nebulak.localhost>
2018-09-13 12:10:08 +0200

    refactor

--- flex-hkp/hkp_server.py
+++ flex-hkp/hkp_server.py
@@ -12,13 +12,19 @@ import os
 import secrets
 import sqlite3
 import re
+import itertools
+import functools
+import gnupg
+from tempfile import mkdtemp
+from shutil import rmtree
 
 app = Flask(__name__)
 
 # Path configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 GPG_HOME = os.path.join(BASE_DIR, 'keystore', 'gpg-home')
-GPG_UNVERIFIED_HOME = os.path.join(BASE_DIR, 'keystore', 'gpg_unverified')
+GPG_UNVERIFIED_KEYRING = os.path.join(BASE_DIR, 'keystore', 'gpg_unverified')
+GPG_MAIN_KEYRING = os.path.join(BASE_DIR, 'keystore', 'gpg_main')
 KEY_STORE = os.path.join(BASE_DIR, 'keystore', 'keys')
 BASE_URL = "http://127.0.0.1"
 
@@ -45,71 +51,188 @@ if not os.path.exists(KEY_STORE):
 	os.makedirs(KEY_STORE, 0o700)
 
 
-# Database functions
-def db_connect(db_path=DB_PATH):
-	if is_db_created(db_path) is False:
-		create_db(db_path)
 
-	con = sqlite3.connect(db_path)
-	return con
-
-def is_db_created(db_path=DB_PATH):
-	os.path.isfile(DB_PATH)
-
-def create_db(db_path=DB_PATH):
-	con = sqlite3.connect(db_path)
-	cur = con.cursor()
-	validation_tokens = """ CREATE TABLE IF NOT EXISTS validation_tokens (
-								id integer PRIMARY KEY,
-								email text NOT NULL,
-								token text NOT NULL,
-								created_at text NOT NULL
-							); """
-	cur.execute(validation_tokens)
-
-
-def create_validation_link(email, db_path=DB_PATH):
-
-	insert_token_sql = "INSERT INTO validation_tokens (email, token, created_at) VALUES (?, ?, ?)"
-	import datetime
-	import base64
-	validation_token = token_urlsafe(64)
-	now = datetime.now().isoformat(' ', 'seconds')
-	con = db_connect()
-	cur.execute(insert_token_sql, (email, validation_token, now))
-	return "".join(BASE_URL, '/verify?email=', base64.b64encode(email), '&token=', validation_token )
 
 # //TODO: classes
 # //TODO: class MainKeyRing(add, update, delete, does key for email exist, list)
-# //TODO: class KeyInspector(is_upload_key, get_key_domain, is_key_domain_valid, get_key_email_address, get_localpart_zb32)
 # //TODO: class WKDFilestore(add, update, delete, does key for email exist, list)
-# //TODO: class HKPTools (zb32_encode, zb32_decode)
-# //TODO: class HKPMailer (send)
+# //TODO: combine the two above classes in class KeyStore
 
-# Email functions
-def send_mail(recipients, message):
-	import smtplib
+class KeyInspector(object):
+	def __init__(self, armored_key, temp_path):
+		self.armored_key = armored_key
+		self.email = ""
+		self.domain = ""
+		self.local_part = ""
+		self.keyring_path = ""
+		self.is_openpgp_key = None
+		self.temp_keyring_path = ""
+		self.gpg = None
 
-	message_head = """From: From Person <from@fromdomain.com>
-	To: To Person <to@todomain.com>
-	Subject: Please verify your uploaded OpenPGP-Key
+		# build a temporary place for empty keyring
+		self.temp_keyring_path = mkdtemp(prefix = temp_path)
 
-	"""
+		# Init the GPG
+		self.gpg = gnupg.GPG(gnupghome = self.temp_keyring_path, options = [
+			'--with-colons',
+			'--keyid-format=LONG',
+			'--export-options=export-minimal,export-clean,no-export-attributes',
+			'--import-options=import-minimal,import-clean'
+			], verbose = False)
 
-	complete_message = message_head.join(message)
+		# Blindly try to import and check result. If we have count we are fine
+		import_result = self.gpg.import_keys(self.armored_key)
+		if import_result.count <= 0:
+			self.is_openpgp_key = False
+		self.is_openpgp_key = True
 
-	try:
-		smtpObj = smtplib.SMTP(SMTP_HOST)
-		smtpObj.sendmail(SMTP_SENDER, recipients, complete_message)
-		print ("Successfully sent email")
+	def __del__(self):
+		rmtree(self.temp_keyring_path)
 
-		# set up the SMTP server
-		s = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
-		s.starttls()
-		s.login(SMTP_USER, SMTP_PASSWORD)
-	except SMTPException:
-	    print ("Error: unable to send email")
+	def is_openpgp_key():
+		return self.is_openpgp_key
 
+	def is_valid_domain(domain_list):
+		self.get_address_info()
+		is_key_valid_domain = False
+		for domain in domain_list:
+			if domain == self.domain:
+				is_key_valid_domain = True
+				break
+		return is_key_valid_domain
+
+	def get_address_info():
+		imported_keys = self.gpg.list_keys()
+		is_key_valid_domain = False
+		for key in imported_keys:
+			for uid in key.get('uids', []):
+                address = email.utils.parseaddr(uid)[1]
+				if '@' not in addr:
+                    continue
+				self.email = address.lower()
+				self.local_part, self.domain = self.email.split("@", 1)
+
+		return self.email, self.local_part, self.domain
+
+
+class HKPMailer(object):
+	def __init__(self, host, port, username, password, sender, start_tls):
+		self.host = host
+		self.port = port
+		self.username = username
+		self.password = password
+		self.sender = sender
+		self.start_tls = start_tls
+
+	def send(recipients, message):
+		import smtplib
+
+		message_head = """From: From Person <from@fromdomain.com>
+		To: To Person <to@todomain.com>
+		Subject: Please verify your uploaded OpenPGP-Key
+
+		"""
+
+		complete_message = message_head.join(message)
+
+		try:
+			smtpObj = smtplib.SMTP(self.host)
+			smtpObj.sendmail(self.sender, recipients, complete_message)
+			print ("Successfully sent email")
+
+			# set up the SMTP server
+			s = smtplib.SMTP(host=self.host, port=self.port)
+			if self.start_tls:
+				s.starttls()
+			s.login(self.user, self.password)
+		except SMTPException:
+		    print ("Error: unable to send email")
+
+class KeyVerifier(object):
+	def __init__(self, hkp_mailer):
+		self.mailer = hkp_mailer
+
+	# Database functions
+	def db_connect(db_path=DB_PATH):
+		if self.is_db_created(db_path) is False:
+			self.create_db(db_path)
+
+		con = sqlite3.connect(db_path)
+		return con
+
+	def is_db_created(db_path=DB_PATH):
+		os.path.isfile(DB_PATH)
+
+	def create_db(db_path=DB_PATH):
+		con = sqlite3.connect(db_path)
+		cur = con.cursor()
+		validation_tokens = """ CREATE TABLE IF NOT EXISTS validation_tokens (
+									id integer PRIMARY KEY,
+									email text NOT NULL,
+									token text NOT NULL,
+									created_at text NOT NULL
+								); """
+		cur.execute(validation_tokens)
+
+	def get_validation_link(email, db_path=DB_PATH):
+		insert_token_sql = "INSERT INTO validation_tokens (email, token, created_at) VALUES (?, ?, ?)"
+		import datetime
+		import base64
+		validation_token = token_urlsafe(64)
+		now = datetime.now().isoformat(' ', 'seconds')
+		con = db_connect()
+		cur.execute(insert_token_sql, (email, validation_token, now))
+		validation_link = "".join(BASE_URL, '/verify?email=', base64.b64encode(email), '&token=', validation_token )
+		return validation_link
+
+	def send(email, validation_link):
+		self.mailer.send([email], validation_link)
+
+
+class HKPTools:
+	# Source: https://gist.githubusercontent.com/tochev/99f19d9ce062f1c7e203
+	# /raw/0077ec38adc350e0fd1207e6a525de482b40df7e/zbase32.py
+	# Copyright: Tocho Tochev <tocho AT tochev DOT net>
+	# Licence: MIT
+	# See http://philzimmermann.com/docs/human-oriented-base-32-encoding.txt
+	@staticmethod
+	def zb32_encode(bs):
+		"""
+	    Encode bytes bs using zbase32 encoding.
+	    Returns: bytearray
+
+	    >>> encode_zbase32(b'\\xd4z\\x04') == b'4t7ye'
+	    True
+	    """
+	    ALPTHABET = b"ybndrfg8ejkmcpqxot1uwisza345h769"
+	    result = bytearray()
+	    for word in itertools.zip_longest(*([iter(bs)] * 5)):
+	        padding_count = word.count(None)
+	        n = functools.reduce(lambda x, y: (x << 8) + (y or 0), word, 0)
+	        for i in range(0, (40 - 8 * padding_count), 5):
+	            result.append(ALPTHABET[(n >> (35 - i)) & 0x1F])
+	    return result
+
+class KeyRing(object):
+	def __init__(self):
+		pass
+
+	def add(armored_key):
+		pass
+
+class WKDStore(object):
+	def __init__(self):
+		pass
+
+	def add(armored_key):
+		pass
+
+class KeyStore(object):
+	def __init__(self):
+		pass
+
+	def add(armored_key):
+		pass
 
 def get_key_file_path(keyid=''):
 	"""
@@ -236,74 +359,39 @@ def add_key():
 	Add keys that we were sent
 	"""
 
-	import gnupg
-	from tempfile import mkdtemp
-	from shutil import rmtree
+	key_inspector = KeyInspector(request.form['keytext'])
 
-	# build a temporary place for empty keyring
-	_gpghome = mkdtemp(prefix = os.path.join(GPG_HOME, 'temp'))
-
-	# Init the GPG
-	gpg = gnupg.GPG(gnupghome = _gpghome, options = [
-		'--with-colons',
-		'--keyid-format=LONG',
-		'--export-options=export-minimal,export-clean,no-export-attributes',
-		'--import-options=import-minimal,import-clean'
-		], verbose = False)
-
-
-	# Blindly try to import and check result. If we have count we are fine
-	import_result = gpg.import_keys(request.form['keytext'])
-	if import_result.count <= 0:
-		rmtree(_gpghome)
+	if key_inspector.is_openpgp_key() is False:
 		return return_error(501, 'Invalid key sent')
 
 	# check for valid domain
 	if RESTRICT_DOMAIN:
-		imported_keys = gpg.list_keys()
-		is_key_valid_domain = False
-		for key in imported_keys:
-			username, email_domain = key['uids'][0].split("@", 1)
-			email_domain = email_domain.replace('>', '')
-			email_domain = email_domain.replace(' ', '')
-			print(email_domain)
-			for domain in ALLOWED_DOMAINS:
-				if domain == email_domain:
-					is_key_valid_domain = True
-					break
-			if is_key_valid_domain:
-				break
-		if is_key_valid_domain == False:
-			rmtree(_gpghome)
+		if key_inspector.is_valid_domain(ALLOWED_DOMAINS) == False:
 			return return_error(501, 'Invalid key sent. Domain is not allowed.')
-
 
 	# is email verification enabled
 	if VERIFY_EMAIL:
 		# get email-address from key
-		email_addresses = []
-		imported_keys = gpg.list_keys()
-		for key in imported_keys:
-			email_addresses = re.findall('\S+@\S+', key['uid'])
-			if len(email_addresses) >= 1 or len(email_addresses) <= 0:
-				rmtree(_gpghome)
-				return return_error(501, 'Invalid key sent. No email-address in UID or more than one email-address in UID.')
-
-		email_address = email_addresses[0]
+		_email, _local, _domain = key_inspector.get_address_info()
+		if _email is "":
+			return return_error(501, 'Invalid key sent. No email-address in UID or more than one email-address in UID.')
 
 		# generate and safe validation token in database
-		validation_link = create_validation_link(email_address)
+		mailer = HKPMailer(
+					SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD
+					SMTP_SENDER, SMTP_STARTTLS
+		)
+		verifier = KeyVerifier(mailer)
+		validation_link = verifier.get_validation_link(_email)
 
 		# encrypt validation link
-		openpgp_recipients = []
-		openpgp_recipients.append(email_address)
-		encrypted_validation_link = gpg.encrypt(validation_link, openpgp_recipients)
+		encrypted_validation_link = gpg.encrypt(validation_link, [_email])
 
 		# send encrypted validation_link to uploader
-		send_mail(openpgp_recipients, encrypted_validation_link)
+		verifier.send(_email, encrypted_validation_link)
 
 		# save in "unverified"-keyring
-		gpg_unverified = gnupg.GPG(gnupghome = GPG_UNVERIFIED_HOME, options = [
+		gpg_unverified = gnupg.GPG(gnupghome = GPG_UNVERIFIED_KEYRING, options = [
 			'--with-colons',
 			'--keyid-format=LONG',
 			'--export-options=export-minimal,export-clean,no-export-attributes',
